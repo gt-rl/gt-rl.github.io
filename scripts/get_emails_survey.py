b9835407197b455f35b00e7b5c6ee6cd3d79f560
Bastian Rieck <bastian@rieck.me>
2022-11-28 14:18:50 +0100

    Preparing message

--- scripts/get_emails_survey.py
+++ scripts/get_emails_survey.py
@@ -1,4 +1,4 @@
-"""Get e-mails for authors that allow contact."""
+"""Send e-mails to authors that allow contact."""
 
 from dotenv import load_dotenv
 
@@ -16,30 +16,37 @@ if __name__ == "__main__":
         password=os.getenv("OPENREVIEW_PASSWORD"),
     )
 
-    submissions = client.get_notes(
+     submissions = client.get_notes(
         invitation="logconference.io/LOG/2022/Conference/-/Blind_Submission",
         details="directReplies",
-    )
+     )
 
-    author_ids = set()
-    index = 0
+     author_ids = set()
+     index = 0
 
-    for submission in submissions:
+     for submission in submissions:
         index += 1
         if "Agreement" not in submission.content:
             continue
 
         author_ids.update(submission.content["authorids"])
 
-    print(f"Got {index} submissions", file=sys.stderr)
+     print(f"Got {index} submissions", file=sys.stderr)
 
-    e_mails = set([a for a in author_ids if "@" in a])
+     e_mails = set([a for a in author_ids if "@" in a])
 
-    profiles = client.search_profiles(ids=[a for a in author_ids if a.startswith("~")])
-    for profile in profiles:
+     profiles = client.search_profiles(ids=[a for a in author_ids if a.startswith("~")])
+     for profile in profiles:
         e_mails.update([profile.get_preferred_email()])
 
-    print(f"Got {len(e_mails)} e-mail addresses", file=sys.stderr)
+     print(f"Got {len(e_mails)} e-mail addresses", file=sys.stderr)
 
-    for addr in sorted(e_mails):
-        print(addr)
+    with open(sys.argv[1]) as f:
+        message = f.read()
+
+    client.post_message(
+            subject="LoG: Survey for authors",
+        recipients=sorted(e_mails),
+        message=message,
+        replyTo="bastian.rieck@helmholtz-muenchen.de",
+    )
