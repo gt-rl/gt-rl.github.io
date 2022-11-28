23451ac00c1e61042882dec204d41ee06c063735
Bastian Rieck <bastian@rieck.me>
2022-11-28 14:25:53 +0100

    Are you sure, Dave?

--- scripts/get_emails_survey.py
+++ scripts/get_emails_survey.py
@@ -16,36 +16,42 @@ if __name__ == "__main__":
         password=os.getenv("OPENREVIEW_PASSWORD"),
     )
 
-     submissions = client.get_notes(
+    submissions = client.get_notes(
         invitation="logconference.io/LOG/2022/Conference/-/Blind_Submission",
         details="directReplies",
-     )
+    )
 
-     author_ids = set()
-     index = 0
+    author_ids = set()
+    index = 0
 
-     for submission in submissions:
+    for submission in submissions:
         index += 1
         if "Agreement" not in submission.content:
             continue
 
         author_ids.update(submission.content["authorids"])
 
-     print(f"Got {index} submissions", file=sys.stderr)
+    print(f"Got {index} submissions", file=sys.stderr)
 
-     e_mails = set([a for a in author_ids if "@" in a])
+    e_mails = set([a for a in author_ids if "@" in a])
 
-     profiles = client.search_profiles(ids=[a for a in author_ids if a.startswith("~")])
-     for profile in profiles:
+    profiles = client.search_profiles(
+        ids=[a for a in author_ids if a.startswith("~")]
+    )
+    for profile in profiles:
         e_mails.update([profile.get_preferred_email()])
 
-     print(f"Got {len(e_mails)} e-mail addresses", file=sys.stderr)
+    print(f"Got {len(e_mails)} e-mail addresses", file=sys.stderr)
 
     with open(sys.argv[1]) as f:
         message = f.read()
 
+    # Make sure that we don't spam too much.
+    if not input("Are you sure? (y/n): ").lower().strip()[:1] == "y":
+        sys.exit(1)
+
     client.post_message(
-            subject="LoG: Survey for authors",
+        subject="LoG: Survey for authors",
         recipients=sorted(e_mails),
         message=message,
         replyTo="bastian.rieck@helmholtz-muenchen.de",
