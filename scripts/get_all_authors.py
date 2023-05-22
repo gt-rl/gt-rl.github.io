d98d3726df7fd1c901e6325548de2e047b3572ae
Bastian Rieck <bastian@rieck.me>
2023-05-22 16:00:03 +0200

    Formatting e-mails

--- scripts/get_all_authors.py
+++ scripts/get_all_authors.py
@@ -56,16 +56,16 @@ if __name__ == "__main__":
             n_found_rejected += 1
 
         author_ids = note.content["authorids"]
-        print(note.content["title"])
+        #print(note.content["title"])
 
         for author_id in author_ids:
             if "@" in author_id:
-                print(" ", author_id)
+                print(author_id)
             elif author_id.startswith("~"):
                 profile = client.search_profiles(ids=[author_id])[0]
                 email = profile.content.get(
                     "preferredEmail", profile.content["emails"][0]
                 )
-                print(" ", email)
+                print(email)
 
     assert n_rejected == n_found_rejected
