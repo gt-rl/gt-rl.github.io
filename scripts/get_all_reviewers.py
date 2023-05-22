4a70c50ce715f45c0f9712e67838b55a06477274
Bastian Rieck <bastian@rieck.me>
2023-05-22 16:00:26 +0200

    Updated reviewer script

--- scripts/get_all_reviewers.py
+++ scripts/get_all_reviewers.py
@@ -13,39 +13,45 @@ load_dotenv()
 
 def get_full_name(profile):
     """Get full name from profile."""
-    for name in profile.content['names']:
-        if 'preferred' not in name.keys() or name['preferred']:
-            if name['middle']:
-                return ' '.join([
-                    name['first'], name['middle'], name['last']
-                ])
+    for name in profile.content["names"]:
+        if "preferred" not in name.keys() or name["preferred"]:
+            if name["middle"]:
+                return " ".join([name["first"], name["middle"], name["last"]])
             else:
-                return ' '.join([
-                    name['first'], name['last']
-                ])
+                return " ".join([name["first"], name["last"]])
 
-    return ''
+    return ""
 
 
-if __name__ == '__main__':
-
-    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
-
+if __name__ == "__main__":
     client = openreview.Client(
-        baseurl='https://api.openreview.net',
-        username=os.getenv('OPENREVIEW_USER'),
-        password=os.getenv('OPENREVIEW_PASSWORD')
+        baseurl="https://api.openreview.net",
+        username=os.getenv("OPENREVIEW_USER"),
+        password=os.getenv("OPENREVIEW_PASSWORD"),
     )
 
-    reviewers = []
-    addresses = []
+    group = client.get_group("logconference.io/LOG/2022/Conference/Reviewers")
+    for member in group.members:
+        if "@" in member:
+            print(",".join([member] * 3))
+            continue
 
-    for id in ['ICLR.cc/2021/Workshop/GTRL/Reviewers',
-               'NeurIPS.cc/2020/Workshop/TDA_and_Beyond/Reviewers']:
+        profile = client.search_profiles(ids=[member])[0]
+        name = get_full_name(profile)
+
+        email = profile.content.get("preferredEmail", profile.content["emails"][0])
+        print(f"{member},{name},{email}")
+
+    raise "heck"
+
+    for id in [
+        "ICLR.cc/2021/Workshop/GTRL/Reviewers",
+        "NeurIPS.cc/2020/Workshop/TDA_and_Beyond/Reviewers",
+    ]:
 
         for member in client.get_group(id=id).members:
             try:
-                if '@' in member:
+                if "@" in member:
                     addresses.append(member)
 
                     # Prevent duplicate profiles
@@ -60,11 +66,9 @@ if __name__ == '__main__':
     for profile in reviewers:
         name = get_full_name(profile)
 
-        email = profile.content.get(
-            'preferredEmail', profile.content['emails'][0]
-        )
+        email = profile.content.get("preferredEmail", profile.content["emails"][0])
 
-        addresses.append(f'{email},{name}')
+        addresses.append(f"{email},{name}")
 
     addressses = set(addresses)
 
